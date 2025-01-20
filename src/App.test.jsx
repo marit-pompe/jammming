d6135d681fe910424fc65c651a28351116aec677
Marit <maritpompe@gmail.com>
2025-01-20 18:38:02 +0100

    Test Suite

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,2 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getAccessTokenFromUrl } from './components/Spotify/Spotify';
+
+test('should extract access token from URL hash', () => {
+    window.location.hash = '#access_token=someAccessToken&token_type=bearer';
+
+    const token = getAccessTokenFromUrl();
+    expect(token).toBe('someAccessToken');
+})
